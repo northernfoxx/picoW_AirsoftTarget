82b1ca3591bb2bda0a86de360eadefee9bb60492
t.fujimaki <ta963jpy@gmail.com>
2023-05-02 13:10:04 +0900

    testpage 1

--- html.h
+++ html.h
@@ -1,7 +1,26 @@
 #ifndef _HTML_H_
 #define _HTML_H_
 
-String htmlMainpage = "";
+String htmlMainpage = "<!DOCTYPE html>\
+<html lang=\"ja\">\
+    <head>\
+        <script>\
+            function ledOn(){\
+                fetch(\"./led\", {\
+                    method: \"POST\",\
+                    cache: \"no-cache\",\
+                    body: {\"led0\":\"on\"}\
+                })\
+            }\
+        </script>\
+    </head>\
+    <body>\
+        <button onclick=\"ledOn\">LEDON</button>\
+        <form action=\"./lighton\">\
+            <input type=\"submit\" value=\"TEST1\">\
+        </form>\
+    </body>\
+</html>";
 
 
 #endif
